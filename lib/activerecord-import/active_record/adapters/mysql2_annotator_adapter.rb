f1e6e3cb5535513da91f14be16d02d3c48365e3b
Oky Sabeni <okysabeni@mavenlink.com>
2018-04-26 17:48:56 -0700

    require typo for ar import
    Signed-off-by: Brad Midgley <bmidgley@mavenlink.com>

--- lib/activerecord-import/active_record/adapters/mysql2_annotator_adapter.rb
+++ lib/activerecord-import/active_record/adapters/mysql2_annotator_adapter.rb
@@ -2,7 +2,7 @@
 # See https://github.com/zdennis/activerecord-import in section `Additional Adapters`
 
 require "active_record/connection_adapters/mysql2_annotator_adapter"
-require "active_record/import/mysql2_adapter"
+require "activerecord-import/adapters/mysql2_adapter"
 
 class ActiveRecord::ConnectionAdapters::Mysql2AnnotatorAdapter
   include ActiveRecord::Import::Mysql2Adapter
